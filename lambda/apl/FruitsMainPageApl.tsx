1c9403c52b9e81d495dd79d240605d5d053d1e93
Tsuneki Umeda <umedat@amazon.co.jp>
2020-09-25 20:19:44 +0900

    Refactor apl/FruitsMainPageApl.tsx

--- lambda/apl/FruitsMainPageApl.tsx
+++ lambda/apl/FruitsMainPageApl.tsx
@@ -3,6 +3,7 @@
 // Licensed under the Amazon Software License  http://aws.amazon.com/asl/
 
 import * as React from 'react'
+import { ViewportProfile } from 'ask-sdk'
 import * as Utils from '../utils'
 import * as FruitSkillMetadata from '../data/fruit_skill_metadata.json'
 import * as FruitInfo from '../data/fruit_info.json'
@@ -16,50 +17,45 @@ import {
 import { AlexaHeader } from 'ask-sdk-jsx-for-apl/alexa-layouts'
 import { FruitsRow } from './FruitsRow'
 
-class FruitsMainPageApl extends React.Component {
-  constructor(props) {
-    super()
-    this.pageProperties = Utils.getPageProperties(props.viewportProfile)
-    this.fruitRows = Utils.splitArray(
-      FruitInfo.data,
-      this.pageProperties.listRowCount
-    )
-  }
-  render() {
-    const isViewportSmall = this.pageProperties.size === 'small'
-    return (
-      <APL theme='dark'>
-        <MainTemplate>
-          <Container width='100vw' height='100vh'>
-            <Image
-              width='100vw'
-              height='100vh'
-              position='absolute'
-              scale='best-fill'
-              source={
-                FruitSkillMetadata.properties.backgroundImageSourceUrl
-              }></Image>
-            <Sequence width='100%' height='100%' scrollDirection='vertical'>
-              {isViewportSmall ? null : (
-                <AlexaHeader
-                  headerTitle={FruitSkillMetadata.properties.headerTitle}
+export const FruitsMainPageApl: React.FC<{
+  viewportProfile: ViewportProfile
+}> = ({ viewportProfile }) => {
+  const pageProperties = Utils.getPageProperties(viewportProfile)
+  const fruitRows = Utils.splitArray(
+    FruitInfo.data,
+    pageProperties.listRowCount
+  )
+
+  const isViewportSmall = pageProperties.size === 'small'
+  return (
+    <APL theme='dark'>
+      <MainTemplate>
+        <Container width='100vw' height='100vh'>
+          <Image
+            width='100vw'
+            height='100vh'
+            position='absolute'
+            scale='best-fill'
+            source={
+              FruitSkillMetadata.properties.backgroundImageSourceUrl
+            }></Image>
+          <Sequence width='100%' height='100%' scrollDirection='vertical'>
+            {isViewportSmall ? null : (
+              <AlexaHeader
+                headerTitle={FruitSkillMetadata.properties.headerTitle}
+              />
+            )}
+            {fruitRows.map(fruitRow => {
+              return (
+                <FruitsRow
+                  fruitRowData={fruitRow}
+                  pageProperties={pageProperties}
                 />
-              )}
-              {this.fruitRows.map(fruitRow => {
-                return (
-                  <FruitsRow
-                    fruitRowData={fruitRow}
-                    pageProperties={this.pageProperties}
-                  />
-                )
-              })}
-            </Sequence>
-          </Container>
-        </MainTemplate>
-      </APL>
-    )
-  }
-}
-module.exports = {
-  FruitsMainPageApl,
+              )
+            })}
+          </Sequence>
+        </Container>
+      </MainTemplate>
+    </APL>
+  )
 }
