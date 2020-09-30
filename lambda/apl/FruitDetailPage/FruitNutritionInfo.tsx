395f42e45f5e815b24a90788987654b442d1a9ce
Tsuneki Umeda <umedat@amazon.co.jp>
2020-09-30 22:39:06 +0900

    Refactor FruitNutritionInfo.tsx

--- lambda/apl/FruitDetailPage/FruitNutritionInfo.tsx
+++ lambda/apl/FruitDetailPage/FruitNutritionInfo.tsx
@@ -6,31 +6,25 @@ import * as React from 'react'
 import * as Utils from '../../utils'
 import { Text } from 'ask-sdk-jsx-for-apl'
 
-class FruitNutritionInfo extends React.Component {
-  constructor(props) {
-    super()
-    console.log('FruitNutritionInfo ', props.fruitName)
-    this.fruitInfo = Utils.getFruitInfo(props.fruitName)
-  }
-  render() {
-    return Object.keys(this.fruitInfo.nutritionInfo).map(nutritionName => {
-      const nutritionValue = this.fruitInfo.nutritionInfo[nutritionName]
-      return (
-        <Text
-          width='100%'
-          height='52dp'
-          color='#F8F8FF'
-          paddingTop='12dp'
-          paddingBottom='12dp'
-          textAlign='center'
-          textAlignVertical='center'
-          fontSize='22dp'
-          text={`${nutritionName}: ${nutritionValue}`}
-        />
-      )
-    })
-  }
-}
-module.exports = {
-  FruitNutritionInfo,
+export const FruitNutritionInfo: React.FC<{ fruitName: string }> = ({ fruitName }): JSX.Element => {
+  const fruitInfo = Utils.getFruitInfo(fruitName)
+
+  const renderedText = Object.keys(fruitInfo.nutritionInfo).map(nutritionName => {
+    const nutritionValue = fruitInfo.nutritionInfo[nutritionName]
+    return (
+      <Text
+        width='100%'
+        height='52dp'
+        color='#F8F8FF'
+        paddingTop='12dp'
+        paddingBottom='12dp'
+        textAlign='center'
+        textAlignVertical='center'
+        fontSize='22dp'
+        text={`${nutritionName}: ${nutritionValue}`}
+      />
+    )
+  })
+
+  return <div>{renderedText}</div>
 }
